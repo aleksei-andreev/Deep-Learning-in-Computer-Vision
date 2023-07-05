2072943892999c4b3cba542f99a762293211d96b
aleksei-andreev <124316093+aleksei-andreev@users.noreply.github.com>
2023-07-05 17:39:43 +0300

    Change __init__.py config

--- nanodet-main/nanodet/data/dataset/__init__.py
+++ nanodet-main/nanodet/data/dataset/__init__.py
@@ -17,7 +17,7 @@ import warnings
 
 from .coco import CocoDataset
 from .xml_dataset import XMLDataset
-from .yolo import YoloDataset
+# from .yolo import YoloDataset
 
 
 def build_dataset(cfg, mode):
@@ -28,8 +28,8 @@ def build_dataset(cfg, mode):
             "Dataset name coco has been deprecated. Please use CocoDataset instead."
         )
         return CocoDataset(mode=mode, **dataset_cfg)
-    elif name == "yolo":
-        return YoloDataset(mode=mode, **dataset_cfg)
+    # elif name == "yolo":
+    #    return YoloDataset(mode=mode, **dataset_cfg)
     elif name == "xml_dataset":
         warnings.warn(
             "Dataset name xml_dataset has been deprecated. "
@@ -38,8 +38,8 @@ def build_dataset(cfg, mode):
         return XMLDataset(mode=mode, **dataset_cfg)
     elif name == "CocoDataset":
         return CocoDataset(mode=mode, **dataset_cfg)
-    elif name == "YoloDataset":
-        return YoloDataset(mode=mode, **dataset_cfg)
+    # elif name == "YoloDataset":
+    #    return YoloDataset(mode=mode, **dataset_cfg)
     elif name == "XMLDataset":
         return XMLDataset(mode=mode, **dataset_cfg)
     else:
