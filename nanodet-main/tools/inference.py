99787ebef427d577a2c345601c50c7466f533764
aleksei-andreev <124316093+aleksei-andreev@users.noreply.github.com>
2023-07-05 21:25:06 +0300

    Change inference parameters

--- nanodet-main/tools/inference.py
+++ nanodet-main/tools/inference.py
@@ -12,24 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import numpy as np
 import os
 import time
 
 import cv2
 import torch
 
-from nanodet.data.transform import Pipeline
-from nanodet.model.arch import build_model
-from nanodet.util import load_model_weight
+from nanodet-main.nanodet.data.transform import Pipeline
+from nanodet-main.nanodet.model.arch import build_model
+from nanodet-main.nanodet.util import load_model_weight
+from nanodet-main.nanodet.util import cfg
+from nanodet-main.nanodet.data.collate import naive_collate
+from nanodet-main.nanodet.data.batch_process import stack_batch_img
 
 
 class Predictor(object):
-    def __init__(self, cfg, model_path, logger, device="cuda:0"):
+    # def __init__(self, cfg, model_path, logger, device="cuda:0"):
+    def __init__(self, cfg, model_path, confidence_minimum):
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         self.cfg = cfg
         self.device = device
         model = build_model(cfg.model)
         ckpt = torch.load(model_path, map_location=lambda storage, loc: storage)
-        load_model_weight(model, ckpt, logger)
+        # load_model_weight(model, ckpt, logger)
+        load_model_weight(model, ckpt)
         if cfg.model.arch.backbone.name == "RepVGG":
             deploy_config = cfg.model
             deploy_config.arch.backbone.update({"deploy": True})
@@ -39,28 +46,37 @@ class Predictor(object):
             model = repvgg_det_model_convert(model, deploy_model)
         self.model = model.to(device).eval()
         self.pipeline = Pipeline(cfg.data.val.pipeline, cfg.data.val.keep_ratio)
+        self.confidence_minimum = confidence_minimum
 
     def inference(self, img):
-        img_info = {}
-        if isinstance(img, str):
-            img_info["file_name"] = os.path.basename(img)
-            img = cv2.imread(img)
-        else:
-            img_info["file_name"] = None
+        # img_info = {}
+        img_info = {"id":0}
+        img_info["file_name"] = None
+        # if isinstance(img, str):
+        #     img_info["file_name"] = os.path.basename(img)
+        #     img = cv2.imread(img)
+        # else:
+        #     img_info["file_name"] = None
 
         height, width = img.shape[:2]
         img_info["height"] = height
         img_info["width"] = width
         meta = dict(img_info=img_info, raw_img=img, img=img)
-        meta = self.pipeline(meta, self.cfg.data.val.input_size)
-        meta["img"] = (
-            torch.from_numpy(meta["img"].transpose(2, 0, 1))
-            .unsqueeze(0)
-            .to(self.device)
-        )
+        # meta = self.pipeline(meta, self.cfg.data.val.input_size)
+        meta = self.pipeline(None, meta, self.cfg.data.val.input_size)
+        meta["img"] = torch.from_numpy(meta["img"].transpose(2, 0, 1)).unsqueeze(0).to(self.device)
+        meta = naiva_collate([meta])
+        meta["img"] = stack_batch_img(meta["img"], divisible=32)
         with torch.no_grad():
             results = self.model.inference(meta)
-        return meta, results
+        results = [results[0][lbl] for lbl in results[0] if cfg.class_names[lbl] == "person"][0]
+        results = np.array([pers for pers in results if pers[4] > self.confidence_minimum])
+        results[:, 2] -= results[:, 0]
+        results[:, 3] -= results[:, 1]
+        h, _ = results.shape
+        results = np.c_[-np.ones((h, 2)), results, -np.ones((h, 3))]
+        # return meta, results
+        return results
 
     def visualize(self, dets, meta, class_names, score_thres, wait=0):
         time1 = time.time()
