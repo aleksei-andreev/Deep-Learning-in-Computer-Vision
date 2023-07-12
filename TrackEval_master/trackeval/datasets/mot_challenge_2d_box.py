48f878de7c4b93e0c42ef864a6dc7eb41ca7d486
aleksei-andreev <124316093+aleksei-andreev@users.noreply.github.com>
2023-07-12 23:21:52 +0300

    Edit paths

--- TrackEval_master/trackeval/datasets/mot_challenge_2d_box.py
+++ TrackEval_master/trackeval/datasets/mot_challenge_2d_box.py
@@ -17,8 +17,8 @@ class MotChallenge2DBox(_BaseDataset):
         """Default class config values"""
         code_path = utils.get_code_path()
         default_config = {
-            'GT_FOLDER': os.path.join(code_path, '../mot_dataset/'),  # Location of GT data
-            'TRACKERS_FOLDER': os.path.join(code_path, '../mot_trackers/'),  # Trackers location
+            'GT_FOLDER': os.path.join(code_path, '../MOT/'),  # Location of GT data
+            'TRACKERS_FOLDER': os.path.join(code_path, '../results/'),  # Trackers location
             'OUTPUT_FOLDER': None,  # Where to save eval results (if None, same as TRACKERS_FOLDER)
             'TRACKERS_TO_EVAL': None,  # Filenames of trackers to eval (if None, all in folder)
             'CLASSES_TO_EVAL': ['pedestrian'],  # Valid: ['pedestrian']
