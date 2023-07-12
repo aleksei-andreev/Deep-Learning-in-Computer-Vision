41d4c719ce0576e4cc45ccd912a1729776ccfd95
aleksei-andreev <124316093+aleksei-andreev@users.noreply.github.com>
2023-07-12 19:33:16 +0300

    Edit path

--- YOLOX_main/yolox/models/yolo_head.py
+++ YOLOX_main/yolox/models/yolo_head.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from yolox.utils import bboxes_iou, cxcywh2xyxy, meshgrid, visualize_assign
+from YOLOX_main.yolox.utils import bboxes_iou, cxcywh2xyxy, meshgrid
 
 from .losses import IOUloss
 from .network_blocks import BaseConv, DWConv
